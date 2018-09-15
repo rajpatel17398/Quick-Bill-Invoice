4bf10cce42290e467bceecda456edc5dcf810c3d
rajpa <rajpatel17398@gmail.com>
2018-09-15 13:50:55 +0530

    test

---
+++ app/src/main/java/com/example/rajpa/dashboard/login.java
@@ -0,0 +1,95 @@
+package com.example.rajpa.dashboard;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class login extends AppCompatActivity {
+    EditText e1,e2;
+    Button b1;
+    TextView t1;
+
+    String URL= "https://rajpatel17398.000webhostapp.com/login.php";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        e1=(EditText)findViewById(R.id.editText);
+        e2=(EditText)findViewById(R.id.editText2);
+
+        b1=(Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String mail=e1.getText().toString();
+                final String pass=e2.getText().toString();
+
+                StringRequest request=new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if (response.trim().equals("success")){
+                            Toast.makeText(login.this, "Success", Toast.LENGTH_SHORT).show();
+                            Intent intent=new Intent(login.this,navigation_dashboard.class);
+                            startActivity(intent);
+                        }else {
+                            Toast.makeText(login.this, "Something Wrong", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(login.this, error.toString(), Toast.LENGTH_SHORT).show();
+
+                    }
+                })
+                {
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String,String>param=new HashMap<>();
+                        param.put("email",mail);
+                        param.put("password",pass);
+                        return param;
+                    }
+                };
+                RequestQueue queue= Volley.newRequestQueue(login.this);
+                queue.add(request);
+
+            }
+
+
+        });
+    }
+}
