64344306e0c10310f8990e299dd201d075b392ad
jp <rajpatel17398@gmail.com>
2018-10-13 16:01:36 +0530

    jp

--- app/src/main/java/com/example/rajpa/dashboard/Activity/Buy.java
+++ app/src/main/java/com/example/rajpa/dashboard/Activity/Buy.java
@@ -3,6 +3,8 @@ package com.example.rajpa.dashboard.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -13,11 +15,24 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.rajpa.dashboard.R;
 
-public class Buy extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Buy extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     GridLayout g1;
-    Spinner s1,s2,s3,s4;
+    static Spinner s1,s2,s3,s4;
     Button b1,b2;
     TextView kg,rs,insurence,gst,total;
     EditText weight,price,insu;
@@ -25,13 +40,19 @@ public class Buy extends AppCompatActivity {
     RadioGroup rg;
     RadioButton r1,r2;
     double t,w,p;
+    List<String>party_list=new ArrayList<>();
+    List<String>quality_list=new ArrayList<>();
+    String choose_quality_URL="https://rajpatel17398.000webhostapp.com/buy%20fetch.php?choose_Quality=choose_Quality";
+    String choose_party_URL="https://rajpatel17398.000webhostapp.com/buy%20fetch.php?choose_party=choose_party";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy);
         g1=(GridLayout)findViewById(R.id.grid);
         s1=(Spinner)findViewById(R.id.choose_party);
+        s1.setOnItemSelectedListener(this);
         s2=(Spinner)findViewById(R.id.choose_Quality);
+        s2.setOnItemSelectedListener(this);
         s3=(Spinner)findViewById(R.id.choose_bf);
         s4=(Spinner) findViewById(R.id.choose_gsm);
         b1=(Button) findViewById(R.id.buton);
@@ -49,7 +70,10 @@ public class Buy extends AppCompatActivity {
         r1=(RadioButton)findViewById(R.id.r1);
         r2=(RadioButton) findViewById(R.id.r2);
 
-        price.setOnClickListener(new View.OnClickListener() {
+        choose_party_spinner();
+        choose_quality_spinner();
+
+    /*    price.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 w = Double.parseDouble(weight.getText().toString());
@@ -58,7 +82,93 @@ public class Buy extends AppCompatActivity {
                 total.setText(Double.toString(t));
                 Toast.makeText(Buy.this, "hi", Toast.LENGTH_SHORT).show();
             }
+        });*/
+
+    }
+
+    private void choose_quality_spinner() {
+        StringRequest request1=new StringRequest(Request.Method.GET, choose_quality_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONArray array=new JSONArray(response);
+                    for (int i=0;i<array.length();i++){
+                        JSONObject object=array.getJSONObject(i);
+                        String quality=object.getString("quality");
+
+                        quality_list.add(quality);
+                    }
+                    ArrayAdapter aa1= new ArrayAdapter(Buy.this,android.R.layout.simple_spinner_item,quality_list);
+                    aa1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    s2.setAdapter(aa1);
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Buy.this, error.toString(), Toast.LENGTH_SHORT).show();
+
+            }
         });
+        RequestQueue queue1=Volley.newRequestQueue(Buy.this);
+        queue1.add(request1);
+    }
+
+
+    private void choose_party_spinner() {
+
+        StringRequest request=new StringRequest(Request.Method.GET, choose_party_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONArray array=new JSONArray(response);
+                    for (int i=0;i<array.length();i++){
+                        JSONObject object=array.getJSONObject(i);
+                        String company_name=object.getString("company_name");
+
+                        party_list.add(company_name);
+                     }
+                    ArrayAdapter aa = new ArrayAdapter(Buy.this,android.R.layout.simple_spinner_item,party_list);
+                    aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    s1.setAdapter(aa);
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Buy.this, error.toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        RequestQueue queue=Volley.newRequestQueue(Buy.this);
+        queue.add(request);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Spinner spinner = (Spinner) adapterView;
+        if(spinner.getId() == R.id.choose_party)
+        {
+            Toast.makeText(this, party_list.get(i), Toast.LENGTH_SHORT).show();
+        }
+        else if(spinner.getId() == R.id.choose_Quality)
+        {
+            Toast.makeText(this, quality_list.get(i), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
