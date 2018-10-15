63d60a8e645a700df0fa4be154c27d1e0a4c0338
rajpa <rajpatel17398@gmail.com>
2018-10-15 23:02:23 +0530

    sell.java file's all spinner done

--- app/src/main/java/com/example/rajpa/dashboard/Activity/Sell.java
+++ app/src/main/java/com/example/rajpa/dashboard/Activity/Sell.java
@@ -2,14 +2,231 @@ package com.example.rajpa.dashboard.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.GridLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.rajpa.dashboard.R;
 
-public class Sell extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.example.rajpa.dashboard.R;
+
+public class Sell extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    GridLayout g1;
+    static Spinner s1,s2,s3,s4;
+    Button b1;
+    List<String>party_list=new ArrayList<>();
+    List<String>quality_list=new ArrayList<>();
+    List<String>bf_list=new ArrayList<>();
+    List<String>gsm_list=new ArrayList<>();
+    String choose_quality_URL="https://rajpatel17398.000webhostapp.com/sell%20fetch.php?choose_Quality=choose_Quality";
+    String choose_party_URL="https://rajpatel17398.000webhostapp.com/sell%20fetch.php?choose_party=choose_party";
+    String choose_bf_URL="https://rajpatel17398.000webhostapp.com/sell%20fetch.php?choose_bf=choose_bf";
+    String choose_gsm_URL="https://rajpatel17398.000webhostapp.com/sell%20fetch.php?choose_gsm=choose_gsm";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sell);
+        g1=(GridLayout)findViewById(R.id.grid);
+        s1=(Spinner)findViewById(R.id.choose_party);
+        s1.setOnItemSelectedListener(this);
+        s2=(Spinner)findViewById(R.id.choose_Quality);
+        s2.setOnItemSelectedListener(this);
+        s3=(Spinner)findViewById(R.id.choose_bf);
+        s4=(Spinner) findViewById(R.id.choose_gsm);
+        b1=(Button) findViewById(R.id.buton);
+        choose_party_spinner();
+        choose_quality_spinner();
+        choose_bf_spinner();
+        choose_gsm_spinner();
+
+
+    }
+
+    private void choose_quality_spinner() {
+        StringRequest request1=new StringRequest(Request.Method.GET, choose_quality_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONArray array=new JSONArray(response);
+                    quality_list.add("Choose_quality");
+                    for (int i=0;i<array.length();i++){
+                        JSONObject object=array.getJSONObject(i);
+                        String quality=object.getString("quality");
+
+                        quality_list.add(quality);
+                    }
+                    ArrayAdapter aa1= new ArrayAdapter(Sell.this,android.R.layout.simple_spinner_item,quality_list);
+                    aa1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    s2.setAdapter(aa1);
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Sell.this, error.toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        RequestQueue queue1=Volley.newRequestQueue(Sell.this);
+        queue1.add(request1);
+    }
+
+    private void choose_bf_spinner() {
+        StringRequest request2=new StringRequest(Request.Method.GET, choose_bf_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONArray array=new JSONArray(response);
+                    bf_list.add("Choose_bf");
+                    for (int i=0;i<array.length();i++){
+                        JSONObject object=array.getJSONObject(i);
+                        String bf=object.getString("bf");
+
+                        bf_list.add(bf);
+                    }
+                    ArrayAdapter aa2= new ArrayAdapter(Sell.this,android.R.layout.simple_spinner_item,bf_list);
+                    aa2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    s3.setAdapter(aa2);
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Sell.this, error.toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        RequestQueue queue1=Volley.newRequestQueue(Sell.this);
+        queue1.add(request2);
+    }
+
+    private void choose_gsm_spinner() {
+        StringRequest request3=new StringRequest(Request.Method.GET, choose_gsm_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONArray array=new JSONArray(response);
+                    gsm_list.add("Choose_gsm");
+                    for (int i=0;i<array.length();i++){
+                        JSONObject object=array.getJSONObject(i);
+                        String gsm=object.getString("gsm");
+
+                        gsm_list.add(gsm);
+                    }
+                    ArrayAdapter aa3= new ArrayAdapter(Sell.this,android.R.layout.simple_spinner_item,gsm_list);
+                    aa3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    s4.setAdapter(aa3);
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Sell.this, error.toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        RequestQueue queue1=Volley.newRequestQueue(Sell.this);
+        queue1.add(request3);
+    }
+
+
+    private void choose_party_spinner() {
+
+        StringRequest request=new StringRequest(Request.Method.GET, choose_party_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONArray array=new JSONArray(response);
+                    party_list.add("Choose_Company");
+                    for (int i=0;i<array.length();i++){
+                        JSONObject object=array.getJSONObject(i);
+
+                        String company_name=object.getString("company_name");
+                        party_list.add(company_name);
+                    }
+                    ArrayAdapter aa = new ArrayAdapter(Sell.this,android.R.layout.simple_spinner_item,party_list);
+                    aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    s1.setAdapter(aa);
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Sell.this, error.toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        RequestQueue queue=Volley.newRequestQueue(Sell.this);
+        queue.add(request);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Spinner spinner = (Spinner) adapterView;
+        if(spinner.getId() == R.id.choose_party)
+        {
+            Toast.makeText(this, party_list.get(i), Toast.LENGTH_SHORT).show();
+        }
+        else if(spinner.getId() == R.id.choose_Quality)
+        {
+            Toast.makeText(this, quality_list.get(i), Toast.LENGTH_SHORT).show();
+        }
+        else if(spinner.getId() == R.id.choose_bf)
+        {
+            Toast.makeText(this, bf_list.get(i), Toast.LENGTH_SHORT).show();
+        }
+        else if(spinner.getId() == R.id.choose_gsm)
+        {
+            Toast.makeText(this, gsm_list.get(i), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
