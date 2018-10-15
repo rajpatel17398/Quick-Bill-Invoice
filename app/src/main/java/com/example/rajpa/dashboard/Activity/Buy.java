44928f9acdc7c4580d15bbd8b315fc4997c20392
rajpa <rajpatel17398@gmail.com>
2018-10-15 14:19:53 +0530

    buy.java file's all spinner done

--- app/src/main/java/com/example/rajpa/dashboard/Activity/Buy.java
+++ app/src/main/java/com/example/rajpa/dashboard/Activity/Buy.java
@@ -42,8 +42,13 @@ public class Buy extends AppCompatActivity implements AdapterView.OnItemSelected
     double t,w,p;
     List<String>party_list=new ArrayList<>();
     List<String>quality_list=new ArrayList<>();
+    List<String>bf_list=new ArrayList<>();
+    List<String>gsm_list=new ArrayList<>();
     String choose_quality_URL="https://rajpatel17398.000webhostapp.com/buy%20fetch.php?choose_Quality=choose_Quality";
     String choose_party_URL="https://rajpatel17398.000webhostapp.com/buy%20fetch.php?choose_party=choose_party";
+    String choose_bf_URL="https://rajpatel17398.000webhostapp.com/buy%20fetch.php?choose_bf=choose_bf";
+            String choose_gsm_URL="https://rajpatel17398.000webhostapp.com/buy%20fetch.php?choose_gsm=choose_gsm";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -72,6 +77,8 @@ public class Buy extends AppCompatActivity implements AdapterView.OnItemSelected
 
         choose_party_spinner();
         choose_quality_spinner();
+        choose_bf_spinner();
+        choose_gsm_spinner();
 
     /*    price.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -92,6 +99,7 @@ public class Buy extends AppCompatActivity implements AdapterView.OnItemSelected
             public void onResponse(String response) {
                 try {
                     JSONArray array=new JSONArray(response);
+                    quality_list.add("Choose_quality");
                     for (int i=0;i<array.length();i++){
                         JSONObject object=array.getJSONObject(i);
                         String quality=object.getString("quality");
@@ -119,6 +127,74 @@ public class Buy extends AppCompatActivity implements AdapterView.OnItemSelected
         queue1.add(request1);
     }
 
+    private void choose_bf_spinner() {
+        StringRequest request2=new StringRequest(Request.Method.GET, choose_bf_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONArray array=new JSONArray(response);
+                    bf_list.add("Choose_bf");
+                    for (int i=0;i<array.length();i++){
+                        JSONObject object=array.getJSONObject(i);
+                        String bf=object.getString("bf");
+
+                        bf_list.add(bf);
+                    }
+                    ArrayAdapter aa2= new ArrayAdapter(Buy.this,android.R.layout.simple_spinner_item,bf_list);
+                    aa2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    s3.setAdapter(aa2);
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Buy.this, error.toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        RequestQueue queue1=Volley.newRequestQueue(Buy.this);
+        queue1.add(request2);
+    }
+
+    private void choose_gsm_spinner() {
+        StringRequest request3=new StringRequest(Request.Method.GET, choose_gsm_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONArray array=new JSONArray(response);
+                    gsm_list.add("Choose_gsm");
+                    for (int i=0;i<array.length();i++){
+                        JSONObject object=array.getJSONObject(i);
+                        String gsm=object.getString("gsm");
+
+                        gsm_list.add(gsm);
+                    }
+                    ArrayAdapter aa3= new ArrayAdapter(Buy.this,android.R.layout.simple_spinner_item,gsm_list);
+                    aa3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    s4.setAdapter(aa3);
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Buy.this, error.toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        RequestQueue queue1=Volley.newRequestQueue(Buy.this);
+        queue1.add(request3);
+    }
+
 
     private void choose_party_spinner() {
 
@@ -127,10 +203,11 @@ public class Buy extends AppCompatActivity implements AdapterView.OnItemSelected
             public void onResponse(String response) {
                 try {
                     JSONArray array=new JSONArray(response);
+                    party_list.add("Choose_Company");
                     for (int i=0;i<array.length();i++){
                         JSONObject object=array.getJSONObject(i);
-                        String company_name=object.getString("company_name");
 
+                        String company_name=object.getString("company_name");
                         party_list.add(company_name);
                      }
                     ArrayAdapter aa = new ArrayAdapter(Buy.this,android.R.layout.simple_spinner_item,party_list);
@@ -164,6 +241,14 @@ public class Buy extends AppCompatActivity implements AdapterView.OnItemSelected
         else if(spinner.getId() == R.id.choose_Quality)
         {
             Toast.makeText(this, quality_list.get(i), Toast.LENGTH_SHORT).show();
+        }
+        else if(spinner.getId() == R.id.choose_bf)
+        {
+            Toast.makeText(this, bf_list.get(i), Toast.LENGTH_SHORT).show();
+        }
+        else if(spinner.getId() == R.id.choose_gsm)
+        {
+            Toast.makeText(this, gsm_list.get(i), Toast.LENGTH_SHORT).show();
         }
     }
 
